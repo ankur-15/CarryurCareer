82eb6341102c02397853f937b2cc5bf043e2da28
ankur150602 <ankursinghrathore15@gmail.com>
2026-02-11 01:45:14 +0530

    Frontend connected with prediction API

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
-from predictor import predict_colleges
+from backend.predictor import predict_colleges
+
 
 app = FastAPI(title="CarryurCareer – Admission Predictor API")
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
